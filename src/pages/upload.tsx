2972296f470ee4ea530714ac9f3877b17db1bd21
bistii1 <bisti12018@gmail.com>
2025-05-16 17:32:17 -0500

    removed userouter

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 export default function Upload() {
   const { user } = useUser();
